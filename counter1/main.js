be029c4a4ca106f325d45ae195431558dffc8f54
gulzada2003 <gulzadaosk@gmail.com>
2021-11-27 20:29:25 +0600

    delete folders

---
+++ counter1/main.js
@@ -0,0 +1,14 @@
+let number = 0;
+
+function plusOne() {
+  let count = document.getElementById('count');
+  number++;
+  count.textContent = number.toString();
+}
+
+function minusOne() {
+    let count = document.getElementById('counter');
+    number--;
+    count.textContent = number.toString();
+  }
+  
